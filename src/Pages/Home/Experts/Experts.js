ecb4d06e218c102aa6e1f799869a86bd70d9e7d1
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-14 01:00:13 +0600

    expert create

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -6,26 +6,29 @@ import expert4 from '../../../Images/experts/expert-4.jpg';
 import expert5 from '../../../Images/experts/expert-5.jpg';
 import expert6 from '../../../Images/experts/expert-6.png';
 import Expert from '../Expert/Expert';
+const experts = [
+    {id: 1, name: 'Will Smith', img: expert1},
+    {id: 2, name: 'Chris Rock', img: expert2},
+    {id: 3, name: 'Dwayne Rock', img: expert3},
+    {id: 4, name: 'Messy Vai', img: expert4},
+    {id: 5, name: 'Ronaldo Bro', img: expert5},
+    {id: 6, name: 'Stachy Jhonson', img: expert6},
+]
+
 const Experts = () => {
-    const experts = [
-        {id: 1, name: 'Will Smith', img: expert1},
-        {id: 2, name: 'Chris Rock', img: expert2},
-        {id: 3, name: 'Dwayne Rock', img: expert3},
-        {id: 4, name: 'Messy Vai', img: expert4},
-        {id: 5, name: 'Ronaldo Bro', img: expert5},
-        {id: 6, name: 'Stachy Jhonson', img: expert6},
-    ]
+
     return (
-        <div>
-            <h2>Our Experts</h2>
-            <div className="row">
-                {
-                    experts.map(expert => <Expert
-                        key={expert.id}
-                        expert= {expert}
-                    ></Expert>)
-                }
-        </div>
+        <div className='container'>
+            <h2 className='text-primary text-center'>Our Experts</h2>
+            <div className='row'>
+              {
+                  experts.map(expert => <Expert
+                  key={expert.id}
+                  expert ={expert}
+                  ></Expert>)
+              }
+            </div>
+         
         </div>
     );
 };
